653770821183d2a4726549f3219d0b844a6c85ed
mpwow5 <mpwow5@gmail.com>
2022-03-16 18:30:57 +0300

    В файле conftest.py реализована опция выбора языка сайта

--- conftest.py
+++ conftest.py
@@ -1,15 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+'''Функция позволяет получить данные, переданные в командной строке. По умолчанию указываем язык сайта - Английский.'''
 
 
-'''Фикстура позволяет запускать браузер перед каждым тестом и закрывать его после завершения теста'''
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Язык сайта')
+
+
+'''Фикстура позволяет запускать браузер перед каждым тестом и закрывать его после завершения теста.
+Встроенная фикстару request обратывает полученные из командной строки данные'''
 
 
 @pytest.fixture(scope='function')
-def browser():
+def browser(request):
+    user_language = request.config.getoption('language')  # Получаем выбранный язык из командной строки
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print('\nЗапуск браузера')
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     yield browser
     print('\nВыход из браузера')
     browser.quit()
-
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,14 @@
-"""Файл с тестами главной страницы"""
+from pages.main_page import MainPage
 
-link = 'http://selenium1py.pythonanywhere.com/'
+"""Файл с тестами главной страницы"""
 
 '''Тест с проверкой возможности пользователя перейти с главной страницы на страницу логина
 В качестве аргумента функции передается фикстура browser из файла conftest.py'''
 
 
-
-
-
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)  # Инициализуем PageObject, передаем в конструктор экземпляр браузера (из
+    # conftest.py) и url
+    page.open()  # Открываем страницу
+    page.go_to_login_page()  # Выполняем метод страницы (класс BaseObject) и переходим на страницу логина
