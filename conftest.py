f4ffc6ba7c28fa57a1966901114c6893a1e2c596
mpwow5 <mpwow5@gmail.com>
2022-04-04 18:08:10 +0300

    Updated comments in conftest.py

--- conftest.py
+++ conftest.py
@@ -2,24 +2,21 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-'''Функция позволяет получить данные, переданные в командной строке. По умолчанию указываем язык сайта - Английский.'''
-
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en', help='Язык сайта')
+    parser.addoption('--language', action='store', default='en', help='Website language')
 
 
-'''Фикстура позволяет запускать браузер перед каждым тестом и закрывать его после завершения теста.
-Встроенная фикстару request обратывает полученные из командной строки данные'''
+"""Fixture allows open browser before test and close browser after completing test-case"""
 
 
 @pytest.fixture(scope='function')
 def browser(request):
-    user_language = request.config.getoption('language')  # Получаем выбранный язык из командной строки
+    user_language = request.config.getoption('language')
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    print('\nЗапуск браузера')
+    print('\nStarting browser')
     browser = webdriver.Chrome(options=options)
     yield browser
-    print('\nВыход из браузера')
+    print('\nExit browser')
     browser.quit()
