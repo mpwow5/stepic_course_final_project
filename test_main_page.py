335b66c5dc6e7b7cf7a2739d3c6b75c84b00835d
mpwow5 <mpwow5@gmail.com>
2022-04-04 18:02:00 +0300

    Updated comments for main page tests

--- test_main_page.py
+++ test_main_page.py
@@ -3,47 +3,47 @@ from pages.main_page import MainPage
 from pages.login_page import LoginPage
 import pytest
 
-"""Файл с тестами главной страницы"""
+"""File contains tests for main page"""
 
-
-'''Тест проверяет пустая ли корзина при переходе в нее с главной страницы'''
+"""Test checks is the cart empty after going to it from the main page
+    1. Open main page
+    2. Go to basket page
+    3. Basket is empty"""
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)  # Инициализуем PageObject, передаем в конструктор экземпляр браузера (из
-    # conftest.py) и url
-    page.open()  # Открываем страницу
-    page.go_to_basket()  # Переходим в корзину
-    basket_page = BasketPage(browser, browser.current_url)  # Инициализируем PageObject корзины
-    basket_page.is_basket_empty()  # Проверяем пустая ли корзина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.is_basket_empty()
 
 
-"""Класс содержит тесты с проверкой функционала логина пользователя"""
+"""Class contains tests for checking register user"""
 
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
-    """Тест с проверкой возможности пользователя перейти с главной страницы на страницу логина
-    В качестве аргумента функции передается фикстура browser из файла conftest.py"""
+    """Test checks that the user can go from main page to login page
+        1. Open main page
+        2. Go to login page
+        3. Current page have 'login' in URL, register and login form'"""
 
     def test_guest_can_go_to_login_page(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/'
-        page = MainPage(browser, link)  # Инициализуем PageObject, передаем в конструктор экземпляр браузера (из
-        # conftest.py) и url
-        page.open()  # Открываем страницу
-        page.go_to_login_page()  # Переходим на страницу логина
-        login_page = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage, передаем браузер и текущий URL
-        # страницы - должен быть URL страницы логина
-        login_page.should_be_login_page()  # Проверяем наличие login в текущем URL и наличие форм логина и регистрации
-        # пользователя
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-    '''Тест проверяет наличие кнопки логина на основной странице'''
+    '''The test checks for the presence of a login button on the main page
+    1. Open main page
+    2. Login link is presented on main page'''
 
     def test_should_be_login_link(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/'
-        page = MainPage(browser, link)  # Инициализуем PageObject, передаем в конструктор экземпляр браузера (из
-        # conftest.py) и url
-        page.open()  # Открываем страницу
-        page.should_be_login_link()  # Ищем кнопку логина на основной странице
-
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
